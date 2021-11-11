0580b54921f445f44faeb80739839faa4ecef9a2
judithquine <judithquine@yahoo.es>
2021-11-11 12:34:12 +0100

    mostrar elegida

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ console.log("Hello coders");
 
 // intentando crear un Array
 
-let name = ["Anna G", "Ana C", "Laura Campos", "Gabrielle",
+let lista = ["Anna G", "Ana C", "Laura Campos", "Gabrielle",
  "Helen", "Judith", "Sara", "Tamara", "Valentina", "Yuliya", 
  "Alisa", "Candy", "Desirée", "Alexia", "Carmen", 
  "Joana", "Sandra", "Sonia", "Kristina", "Faby", "Grácia", 
- "Laura Contreras", "Marisa", "Rosa"];
-
- 
-
-$(document).ready(function () {
+ "Laura Contreras", "Marisa"];
 
 
+ console.log(lista);
 
-function eligePersona() {
-    
+function eligePersona(){
+  
+  let elegida= lista[Math.floor(Math.random()*lista.length)];
+
+//pintar elegida en la seccion resultado
+$(".resultado").text(elegida)
+  //eliminar a la persona elegida de la lista y guardarla en otra lista
+
 }
+ 
+$(document).ready(function () {
 
 
 

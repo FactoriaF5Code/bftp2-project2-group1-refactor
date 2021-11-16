d4253376b3c37ea4609ed841a3439bdfd7631afd
CrypticiaN <obscura.darkside@gmail.com>
2021-11-16 12:33:22 +0100

    sacar nombre de Array JS

--- main.js
+++ main.js
@@ -8,24 +8,32 @@ let lista = ["Anna G", "Ana C", "Laura Campos", "Gabrielle",
  "Joana", "Sandra", "Sonia", "Kristina", "Faby", "Grácia", 
  "Laura Contreras", "Marisa"];
 
+ lista.sort();
+
 
  console.log(lista);
 
 function eligePersona(){
   
-  let elegida= lista[Math.floor(Math.random()*lista.length)];
+  let number = Math.floor(Math.random()*lista.length);
+
+  let elegida= lista[number];
+  lista.splice(number, 1);
+  console.log(lista);
 
 //pintar elegida en la seccion resultado
-$(".cajaresultado").text(elegida)
-  //eliminar a la persona elegida de la lista y guardarla en otra lista
+$(".cajaresultado").text(elegida);
 
+  //eliminar a la persona elegida de la lista y guardarla en otra lista
+  //lista.splice(elegida, 1);
+  //console.log(lista);
 }
  
 $(document).ready(function () {
 
 
 
-$('.selecciona').on("click", eligePersona)
+$('.selecciona').on("click", eligePersona);
 
 
 

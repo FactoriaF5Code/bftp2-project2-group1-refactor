72f4fe28733409664f7cbbd9521c16539dea45c5
CrypticiaN <obscura.darkside@gmail.com>
2021-11-17 14:44:25 +0100

    roto todo el main.js

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
-$(document).ready(function() {
-
-  // intentando crear un Array
-
-  let persona = ["Anna G", "Ana C", "Laura Campos", "Gabrielle", "Helen", "Judith", "Sara", "Tamara", "Valentina", "Yuliya", 
+// crear Array con lista de personas
+let persona = ["Anna G", "Ana C", "Laura Campos", "Gabrielle", "Helen", "Judith", "Sara", "Tamara", "Valentina", "Yuliya", 
   "Alisa", "Candy", "Desirée", "Alexia", "Carmen",  "Joana", "Sandra", "Sonia", "Kristina", "Faby", "Grácia", 
   "Laura Contreras", "Marisa", "Rosa"];  
   
   persona.sort();
 
-  $(".selecciona").click(function() {
-    $(".elegida").html(eligePersona);
-  });
+
+
+
+
+
+
+
+  
+  //imprimir array en html
+  //for persona.length {
+    //document.appendChild(("personas")).innerHTML = persona;
+  //}
+
+  
+ 
+
+//$(document).ready(function() {
+  //$(".selecciona").click(function() {
+    //$(".elegida").text(eligePersona);
+  //});
   
 
-  function eligePersona(){
-    let number = Math.floor(Math.random()*persona.length);
-    let elegida = persona[number];
+  //function eligePersona(){
+   // let number = Math.floor(Math.random()*persona.length);
+   // let elegida = persona[number];
+    //$(".elegida").text(elegida);
+
+    //iterar sobre todos los items de la lista
+    //while (persona.length <= 23) {
+      //eligePersona(); 
+      //persona.splice(number, 1); 
+      
+      
+     // $(".elegida").html(elegida);
+
+    //console.log(eligePersona);
+   // }
+    
 
     //eliminar a la persona elegida de la lista
-    persona.splice(number, 1);
-    $(".cajaresultado").text(elegida);
+    
 
-    console.log(eligePersona);
-
-    console.log(lista);
-  }
+    //console.log(persona);
+  //}
 
   //pintar elegida en la seccion resultado
   
@@ -34,4 +58,4 @@ $(document).ready(function() {
   //console.log(lista);
     
   
-});
+//});

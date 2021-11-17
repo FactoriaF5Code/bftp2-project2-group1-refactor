9e7a357d05c46667de76667c03df582ab911acbe
judithquine <judithquine@yahoo.es>
2021-11-17 12:09:54 +0100

    funcion eligePersona

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 $(document).ready(function() {
 
-  // intentando crear un Array
+    // intentando crear un Array
 
-  let persona = ["Anna G", "Ana C", "Laura Campos", "Gabrielle", "Helen", "Judith", "Sara", "Tamara", "Valentina", "Yuliya", 
-  "Alisa", "Candy", "Desirée", "Alexia", "Carmen",  "Joana", "Sandra", "Sonia", "Kristina", "Faby", "Grácia", 
-  "Laura Contreras", "Marisa", "Rosa"];  
+    let persona = ["Anna G", "Ana C", "Laura Campos", "Gabrielle", "Helen", "Judith", "Sara", "Tamara", "Valentina", "Yuliya", "Alisa", "Candy", "Desirée", "Alexia", "Carmen",  "Joana", "Sandra", "Sonia", "Kristina", "Faby", "Grácia", "Laura Contreras", "Marisa", "Rosa"];  
   
-  persona.sort();
+    let voluntarias = [];
+    
+    persona.sort();
 
-  $(".selecciona").click(function() {
-    $(".elegida").html(eligePersona);
-  });
+
+    let selectButton = $('.selecciona');
+    selectButton.on("click", eligePersona);
+
+    let cajaResultado = $('.cajaresultado');
+    
+
   
+    function eligePersona(){
+    
+      let number = Math.floor(Math.random()*persona.length);
+      let elegida = persona[number];
 
-  function eligePersona(){
-    let number = Math.floor(Math.random()*persona.length);
-    let elegida = persona[number];
-
-    //eliminar a la persona elegida de la lista
-    persona.splice(number, 1);
-    $(".cajaresultado").text(elegida);
-
-    console.log(eligePersona);
-
-    console.log(lista);
-  }
+      //eliminar a la persona elegida de la lista
+      persona.splice(number, 1);
+      voluntarias.push(elegida);
+    
+      cajaResultado.text(elegida);
+    }
 
   //pintar elegida en la seccion resultado
   
